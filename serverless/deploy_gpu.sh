1853f84e1227d17e8d0819db9c668a59ca272222
Raphael.Pickl <raphael.pickl@ait.ac.at>
2025-03-27 13:16:07 +0000

    finetuned sam

--- serverless/deploy_gpu.sh
+++ serverless/deploy_gpu.sh
@@ -20,6 +20,7 @@ do
         --file "$func_config" --platform local \
         --env CVAT_FUNCTIONS_REDIS_HOST=cvat_redis_ondisk \
         --env CVAT_FUNCTIONS_REDIS_PORT=6666 \
+        --volume `pwd`/serverless/common:/opt/nuclio/common \
         --platform-config '{"attributes": {"network": "cvat_cvat"}}'
 done
 
